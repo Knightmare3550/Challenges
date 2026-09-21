31ddebe7ac79c3efec2e5966b01bb6aed8893e9c
Knightmare3550 <jaime.rodriguez2550@outlook.com>
2026-09-21 12:36:05 -0700

    Mini Challenge #2

---
+++ Mini challenge #2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the first number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int total = number1 + number2;
+Console.WriteLine($"The sum of {number1} and {number2} is {total}.");
