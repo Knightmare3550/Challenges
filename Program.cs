12db8da949b4f38423f7d4905b078efe3f53c18c
Knightmare3550 <jaime.rodriguez2550@outlook.com>
2026-09-21 12:20:13 -0700

    challenge #3

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("What time did you wake up?");
+string wakeUpTime = Console.ReadLine();
+Console.WriteLine($"{name} woke up at {wakeUpTime}.");
